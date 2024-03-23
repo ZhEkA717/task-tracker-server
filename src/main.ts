bf4dc7ed62bd7195a702878354c9dcba5af28acc
Zheka <grushevskiy.yevgeniy@mail.ru>
2024-03-23 12:39:55 +0300

    feat: add env config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = await app.get(ConfigService);
+  const PORT = config.get('PORT') || 3000;
+  await app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+  });
 }
 bootstrap();
